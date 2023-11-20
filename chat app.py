de6f67809d8b58c44b13fa417622b4e96ae3ae99
Abhra Saha <142087481+abhrasaha01@users.noreply.github.com>
2023-11-20 21:33:10 +0530

    Add files via upload

--- chat app.py
+++ chat app.py
@@ -3,11 +3,11 @@ import time
 import threading
 from tkinter import *
  
-root=Tk()
+root=Tk(className="ChatPhrase")
 root.geometry("300x500")
 root.config(bg="white")
  
-def func():
+def threadReceiveMsg():
     t=threading.Thread(target=recv)
     t.start()
  
@@ -24,52 +24,48 @@ def recv():
     (clientsocket,address)=listensocket.accept()
      
     while True:
-        sendermessage=clientsocket.recv(1024).decode()
+        sendermessage=clientsocket.recv(1024).decode()     #utf-8 decoding
         if not sendermessage=="":
             time.sleep(5)
             lstbx.insert(0,"Client : "+sendermessage)
  
  
-s=0
+xr = 0
  
-def sendmsg():
-    global s
-    if s==0:
+def SendMsg():
+    global xr
+    if xr == 0:
         s=socket.socket()
-        hostname=''
+        hostname=''                                       #insert the destination system name
         port=4050
         s.connect((hostname,port))
-        msg=messagebox.get()
-        lstbx.insert(0,"You : "+msg)
-        s.send(msg.encode())
-    else:
-        msg=messagebox.get()
-        lstbx.insert(0,"You : "+msg)
-        s.send(msg.encode())
+        xr += 1
+
+    msg=messagebox.get()
+    lstbx.insert(0,"You : "+msg)
+    s.send(msg.encode())                                   #utf-8 encoding
  
  
-def threadsendmsg():
-    th=threading.Thread(target=sendmsg)
+def threadSendMsg():
+    th=threading.Thread(target=SendMsg)
     th.start()
  
- 
- 
- 
+
 startchatimage=PhotoImage(file='start.png')
  
-buttons=Button(root,image=startchatimage,command=func,borderwidth=0)
+buttons=Button(root,image=startchatimage,command=threadReceiveMsg,borderwidth=0)
 buttons.place(x=90,y=10)
  
 message=StringVar()
-messagebox=Entry(root,textvariable=message,font=('calibre',10,'normal'),border=2,width=32)
+messagebox=Entry(root,textvariable=message,font=('calibre',10,'normal'),border=2,width=32)      #textbox 
 messagebox.place(x=10,y=444)
  
 sendmessageimg=PhotoImage(file='send.png')
  
-sendmessagebutton=Button(root,image=sendmessageimg,command=threadsendmsg,borderwidth=0)
+sendmessagebutton=Button(root,image=sendmessageimg,command=threadSendMsg,borderwidth=0)
 sendmessagebutton.place(x=260,y=440)
  
-lstbx=Listbox(root,height=20,width=43)
+lstbx=Listbox(root,height=20,width=43)                  #entire chat history
 lstbx.place(x=15,y=80)
  
 user_name = Label(root,text =" Number" ,width=10)
